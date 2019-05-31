7a9e408608990b586d68a0d3324213eb29ec6974
Ty Cobb <ty.cobb.m@gmail.com>
2019-05-31 12:36:03 -0500

    ignore dist

--- dist/Scenes/ShowBlog/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ShowBlog_1 = require("./ShowBlog");
-exports.ShowBlog = ShowBlog_1.ShowBlog;
-//# sourceMappingURL=index.js.map
